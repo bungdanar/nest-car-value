7ce8a8bc9534ffebcf4d5192756f6edc04ecca6c
Bung Danar <danar.g.putera@gmail.com>
2021-08-01 20:51:38 +0700

    chore: End of section 9

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repository: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.repository.create({ email, password });
+    return this.repository.save(user);
+  }
+
+  findOne(id: number) {
+    return this.repository.findOne(id);
+  }
+
+  find(email: string) {
+    return this.repository.find({ email });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) throw new NotFoundException('user not found');
+
+    Object.assign(user, attrs);
+    return this.repository.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) throw new NotFoundException('user not found');
+
+    return this.repository.remove(user);
+  }
+}
